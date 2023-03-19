9737246bab8a7ccb4ac1887e09635ecb954e434f
Mohammed Sayed <mohammed.ammar9191@gmail.com>
2023-03-19 13:39:05 +0300

    get and post data in server-side

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,7 +2,7 @@
 projectData = [];
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
@@ -13,20 +13,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
-const port =8080;
+const port = 8080;
 
 // Run the server
 const server = app.listen(port, listen);
 
 // listen function
-function listen(){
-    console.log('server works!')
+function listen() {
+  console.log("server works!");
+}
+
+//get route
+
+app.get("/getData", sendData);
+
+//define callback fun.
+function sendData(req, res) {
+  res.send(projectData);
+  projectData = [];
+}
+
+//post data
+app.post("/add", addData);
+
+//define callback fun.
+function addData(req, res) {
+  console.log(req.body);
+  newData = {
+    date: req.bdy.date,
+    temp: req.body.temp,
+    content: req.body.content,
+  };
+  projectData.push(newData);
 }
